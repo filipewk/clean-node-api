34c35bd226e919ea6d1400bbf21fe6924e29c81b
Filipe <filipe@pop-os.localdomain>
2020-06-07 19:04:46 -0300

    ensure LoginRouter return a valid token when valid credentials are provided

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -81,7 +81,7 @@ describe('Login router', () => {
     expect(httpResponse.body).toEqual(new UnauthorizedError())
   })
   test('Should return 200 when valid credentials are provided', async () => {
-    const { sut } = makeSut()
+    const { sut, authUseCaseSpy } = makeSut()
     const httpRequest = {
       body: {
         email: 'valid_email@mail.com',
@@ -90,6 +90,7 @@ describe('Login router', () => {
     }
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body.accessToken).toEqual(authUseCaseSpy.accessToken)
   })
 
   test('Should return 500 if no AuthUseCase is provided', async () => {
