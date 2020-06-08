f3764c39f6bcbda029deeb20cb279b7600f04ace
Filipe <filipe@pop-os.localdomain>
2020-06-08 20:37:31 -0300

    ensure AuthUseCase throws if no email is provided

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -6,20 +6,20 @@ const makeSut = () => {
 }
 
 describe('Email Validator', () => {
-  test('Should return true if validator returns true', async () => {
+  test('Should return true if validator returns true', () => {
     const sut = makeSut()
     const isEmailValid = sut.isValid('valid_email@mail.com')
     expect(isEmailValid).toBe(true)
   })
 
-  test('Should return false if validator returns false', async () => {
+  test('Should return false if validator returns false', () => {
     validator.isEmailValid = false
     const sut = makeSut()
     const isEmailValid = sut.isValid('invalid_email@mail.com')
     expect(isEmailValid).toBe(false)
   })
 
-  test('Should call validator with correct email', async () => {
+  test('Should call validator with correct email', () => {
     const sut = makeSut()
     sut.isValid('any_email@mail.com')
     expect(validator.email).toBe('any_email@mail.com')
