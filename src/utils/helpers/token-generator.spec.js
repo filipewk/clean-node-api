b1d35a18da3308bb2fd567e9d8b9c6363d26e2a9
Filipe <filipe@pop-os.localdomain>
2020-06-09 22:06:16 -0300

    move sut creatin to a factory helper method

--- src/utils/helpers/token-generator.spec.js
+++ src/utils/helpers/token-generator.spec.js
@@ -1,21 +1,20 @@
 const jwt = require('jsonwebtoken')
+const TokenGenerator = require('./token-generator')
 
-class TokenGenerator {
-  async generate (id) {
-    return jwt.sign(id, 'secret')
-  }
+const makeSut = () => {
+  return new TokenGenerator()
 }
 
 describe('Token Generator', () => {
   test('Should return null if JWT returns null', async () => {
-    const sut = new TokenGenerator()
+    const sut = makeSut()
     jwt.token = null
     const token = await sut.generate('any_id')
     expect(token).toBeNull()
   })
 
   test('Should return a token if JWT returns token', async () => {
-    const sut = new TokenGenerator()
+    const sut = makeSut()
     const token = await sut.generate('any_id')
     expect(token).toBe(jwt.token)
   })
