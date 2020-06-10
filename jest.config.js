20aecb7bd64a7d26ea00a4f6f932745ef36c24d1
Filipe <filipe@pop-os.localdomain>
2020-06-09 23:55:59 -0300

    ensure LoadUserByEmailRepository returns an user if no user is found

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   coverageDirectory: 'coverage',
-  testEnvironment: 'node',
-  collectCoverageFrom: ['**/src/**/*.js']
+  // testEnvironment: 'node',
+  collectCoverageFrom: ['**/src/**/*.js'],
+  preset: '@shelf/jest-mongodb'
 }
