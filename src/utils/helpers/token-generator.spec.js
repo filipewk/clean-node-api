da2b5f5cbb4b876f9900e0b9251b0c0cd549be47
Filipe <filipe@pop-os.localdomain>
2020-06-09 22:15:02 -0300

    Should call JWT with correct values

--- src/utils/helpers/token-generator.spec.js
+++ src/utils/helpers/token-generator.spec.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken')
 const TokenGenerator = require('./token-generator')
 
 const makeSut = () => {
-  return new TokenGenerator()
+  return new TokenGenerator('secret')
 }
 
 describe('Token Generator', () => {
@@ -17,5 +17,12 @@ describe('Token Generator', () => {
     const sut = makeSut()
     const token = await sut.generate('any_id')
     expect(token).toBe(jwt.token)
+  })
+
+  test('Should call JWT with correct value', async () => {
+    const sut = makeSut()
+    await sut.generate('any_id')
+    expect(jwt.id).toBe('any_id')
+    expect(jwt.secret).toBe(sut.secret)
   })
 })
