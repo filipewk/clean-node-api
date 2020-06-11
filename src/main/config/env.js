33140eb33371c87b3317b527d12cd5fa07648993
Filipe <filipe@pop-os.localdomain>
2020-06-11 18:32:40 -0300

    fix app port

--- src/main/config/env.js
+++ src/main/config/env.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
-  tokenSecret: process.env.TOKEN_SECRET || '(*)(^*&^^&*jp34343(76)))'
+  tokenSecret: process.env.TOKEN_SECRET || '(*)(^*&^^&*jp34343(76)))',
+  port: process.env.PORT || 5858
 }
