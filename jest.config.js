b170141d08722040589316476008ba3a00ee45ab
Filipe <filipe@pop-os.localdomain>
2020-06-11 16:07:56 -0300

    ignore module /main in coverage

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
-  collectCoverageFrom: ['**/src/**/*.js'],
+  collectCoverageFrom: ['**/src/**/*.js', '!**/src/main/**.js'],
   preset: '@shelf/jest-mongodb'
 }
