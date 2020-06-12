16615662aa0a7640902d2f0e359dac9782dab0d8
Filipe <filipe@pop-os.localdomain>
2020-06-11 23:11:01 -0300

    return error message instead of error name

--- src/main/config/env.js
+++ src/main/config/env.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://root:filipe50@172.19.0.2:27017/clean-node-api?authSource=admin',
   tokenSecret: process.env.TOKEN_SECRET || '(*)(^*&^^&*jp34343(76)))',
   port: process.env.PORT || 5858
 }
